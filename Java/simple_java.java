104ee448fcf930f1b5ca663be68538cb55991c79
siddhinath <kharadesiddhinath@gmail.com>
2018-09-17 20:02:29 +0530

    adding for loop

--- Java/simple_java.java
+++ Java/simple_java.java
@@ -5,5 +5,10 @@ class simple_java
     public static void main(String args[])
     {
         System.out.println("Siddhinath kharade...");
+        for(int i=0;i<10;i++)
+        {
+            System.out.println("Executing ten times...");
+        }
+
     }
 }
